ba982e23cba127fa9f356d32eb3ccef7121e61a7
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-01-08 16:29:37 -0300

    Using aimation module from matplotlib

--- ideal_gases.py
+++ ideal_gases.py
@@ -1,5 +1,6 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib import animation
 import numpy as np
 from scipy import constants
 import pathlib
@@ -45,49 +46,61 @@ def plot_3d(volume_matrix,
             temperature_matrix,
             pressure_matrix,
             labels=['Volume / m3', 'Temperature / K', 'Pressure / Pa'],
-            save_fig=True,
             save_angle=30):
     """3D plot for pressure, volume and temperature of an ideal gas.
 
-    Parameters
-    ----------
-    volume_matrix : ndarray
-        volume matrix
-    temperature_matrix : ndarray
-        temperature matrix
-    pressure_matrix : ndarray
-        pressure matrix
-    labels : list, optional
-        Axes labels, by default ['Volume / m3', 'Temperature / K',
-        'Pressure / Pa']
-    save_fig : bool, optional
-        If the plot should be saved periodically (an 'images' folder
-        will be created if not exists), by default True
-    save_angle : int, optional
-        The angle interval to save the plot, by default 30
+        Parameters
+        ----------
+        volume_matrix : ndarray
+            volume matrix
+        temperature_matrix : ndarray
+            temperature matrix
+        pressure_matrix : ndarray
+            pressure matrix
+        labels : list, optional
+            Axes labels, by default ['Volume / m3', 'Temperature / K',
+            'Pressure / Pa']
+        save_angle : int, optional
+            The angle interval to save the plot, by default 30
     """
-    fig = plt.figure(1, figsize=(10, 6))
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_wireframe(volume_matrix, temperature_matrix,
-                      pressure_matrix, rstride=50, cstride=50)
+
     ax.set_xlabel(labels[0])
     ax.set_ylabel(labels[1])
     ax.set_zlabel(labels[2])
+    ax.plot_wireframe(volume_matrix, temperature_matrix,
+                      pressure_matrix, rstride=50, cstride=50)
+    # ax.view_init(30, vertical_angle)
 
-    for angle in range(0, 361):
-        ax.view_init(30, angle)
-        plt.draw()
-        plt.pause(.001)
-        if (angle % save_angle == 0) and save_fig:
-            pathlib.Path('images').mkdir(parents=False, exist_ok=True)
-            plt.savefig('images/pvt_plot_{}.png'.format(angle),
-                        bbox_inches='tight')
+    # return fig,
+
+
+def animate(angle, save_angle=30, save_fig=False):
+    ax.view_init(30, angle)
+
+    if (angle % save_angle == 0) and save_fig:
+        pathlib.Path('images').mkdir(parents=False, exist_ok=True)
+        plt.savefig('images/pvt_plot_{}.png'.format(angle),
+                    bbox_inches='tight')
 
 
 if __name__ == "__main__":
-    vol = np.linspace(1, 50, 500)
-    temp = np.linspace(1, 300, 500)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    vol = np.linspace(1, 10, 400)
+    temp = np.linspace(1, 300, 400)
 
     v, t, p = grid_points(vol, temp)
 
     plot_3d(v, t, p)
+
+    anim = animation.FuncAnimation(fig,
+                                   animate,
+                                   frames=361,
+                                   fargs=(30, True),
+                                   interval=1,
+                                   repeat=False)
+
+    plt.show()
+
+    # anim.save('images/animation.mp4')  # to save the animation
